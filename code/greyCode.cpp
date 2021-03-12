a642ba4c0d2aeb87f1bcc7993cde6a58ec63200f
Tawishi <tawishisharma1@gmail.com>
2021-03-12 11:06:02 +0530

    Correction in greyCode

--- code/greyCode.cpp
+++ code/greyCode.cpp
@@ -1,39 +1,41 @@
 #include<bits/stdc++.h>
-
+ 
 using namespace std;
-
-vector <string> geryCode(int n) {
+ 
+vector <string> greyCode(int n) {
     if(n == 0)
         return {"0"};
     if(n == 1) 
-        return {"1","0"};
-
-    vector<string> rev;
+        return {"0","1"};
+ 
+    vector<string> rec = greyCode(n-1);
     vector<string> ans;
-
-    for(int i=n-1; i<=0; i++) {
-        rev.push_back(rev[i]);
+ 
+    // for(int i=n-1; i>=0; i--) {
+    //     rec.push_back(rec[i]);
+    // }
+ 
+    for(int i=0;i<rec.size();i++) {
+        string s = rec[i];
+        ans.push_back("0" + s);
     }
-
-    for(int i=0;i<n;i++) {
-        ans.push_back('0' + rev[i]);
+ 
+    for(int i=rec.size()-1;i>=0;i--) {
+        string s = rec[i];
+        ans.push_back("1" + s);
     }
-
-    for(int i=n;i<2*n;i++) {
-        ans.push_back('1' + rev[i]);
-    }
-
+ 
     return ans;    
 }
-
+ 
 int main()
 {
     int n;
     cin>>n;
     vector<string> s;
-    s = geryCode(n);
-    for(int i=0;i<2*n;i++) {
-        cout<<s[i]<<" ";
+    s = greyCode(n);
+    for(int i=0;i<s.size();i++) {
+        cout<<s[i]<<"\n";
     }
     cout<<"\n";
     return 0;
